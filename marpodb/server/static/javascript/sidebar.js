328b869d0800ee38d241ea89af4500d09a7fa5fb
Mihails Delmans <md656@cam.ac.uk>
2016-10-24 20:45:32 +0100

    Comming back to original Scribl + draworig() modification

--- marpodb/server/static/javascript/sidebar.js
+++ marpodb/server/static/javascript/sidebar.js
@@ -1,7 +1,7 @@
 $(".sidebar-button").click( function(event){
 	event.stopPropagation();
 	$(".top-container").addClass("sidebar-open");
-	console.log("open");
+	// console.log("open");
 });
 
 $(".sidebar").click( function(event){
@@ -12,5 +12,5 @@ $(".pusher").click(function(){
 	if ($(".top-container").hasClass("sidebar-open")){
 		$(".top-container").removeClass("sidebar-open");
 	}
-	console.log("close");
+	// console.log("close");
 });
