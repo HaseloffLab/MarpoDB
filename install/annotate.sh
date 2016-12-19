b6145711fd630ba1ef8bc05b09eb9ce71ea44c7e
Bernardo Pollak <bpollakw@gmail.com>
2016-12-19 12:10:02 +0000

    last changes

--- install/annotate.sh
+++ install/annotate.sh
@@ -13,18 +13,26 @@ blastDB=data/Uniprot/uniprot.fasta
 
 mkdir ${tempDir}
 
+# Blastp search
 blastp -query ${tempDir}/sequences.fa -db data/Uniprot/uniprot.fasta -outfmt "6 qseqid sseqid qlen slen qstart qend sstart send qcovs pident evalue" -evalue 1e-6 -num_threads ${numThreads} > temp/blastOut.outfmt6
 
 python scripts/filterBlastByCl.py ${tempDir}/blastOut.outfmt6 ${cov} ${id} > ${tempDir}/blastOut.filtered.outfmt6
 
 python scripts/addBlastInfo.py ${tempDir}/blastOut.filtered.outfmt6 data/Uniprot/uniprot.info > ${tempDir}/blastp.info
 
-## Might want to parametrise taxonomy!!!
+## Getting Uniprot infos - Might want to parametrise taxonomy!!!
 wget "http://www.uniprot.org/uniprot/?sort=score&desc=&compress=no&query=taxonomy:%22Viridiplantae%20[33090]%22%20AND%20existence:%22evidence%20at%20protein%20level%22%20OR%20existence:%22evidence%20at%20transcript%20level%22&fil=&format=tab&force=yes&columns=id,protein%20names,genes(PREFERRED),genes(ALTERNATIVE),genes(OLN),organism" -O uniprot.info
 
+# Pfam motif search
 hmmpress data/Pfam/Pfam-A.hmm
 
 hmmscan --cpu ${numThreads} --domtblout ${tempDir}/Pfam.domtblout --cut_ga data/Pfam/Pfam-A.hmm temp/sequences.fa
+
+mkdir ${tempDir}/interpro
+
+# InterproScan
+cat ${tempDir}/sequences.fa | sed 's/*//g' > ${tempDir}/sequences_clean.fa
+interproscan.sh -d ${tempDir}/interpro -f gff3 html -goterms -pa -i ${tempDir}/sequences_clean.fa
 
 mkdir data/filtered
 cp temp/Pfam.domtblout data/filtered/Pfam.domtblout
