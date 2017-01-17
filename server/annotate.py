85f0e7ed76220eaaa2c5ac506556818dac761772
Bernardo Pollak <bpollakw@gmail.com>
2017-01-17 12:51:32 +0000

    more cleanup and cleanup scripts

--- server/annotate.py
+++ server/annotate.py
@@ -4,5 +4,5 @@ from tables import *
 
 marpodb = PartsDB('postgresql:///'+sys.argv[1], Base = Base)
 
-#marpodb.annotate('blastphit', fileName = 'data/blastp.info')
+marpodb.annotate('blastphit', fileName = 'data/blastp.info')
 marpodb.annotate('pfamhit', fileName = 'data/Pfam.domtblout')
