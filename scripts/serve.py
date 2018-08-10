921a61056adca96e3940cf07fbbbbd8b3a48db1a
Mihails Delmans <md656@cam.ac.uk>
2018-08-10 17:23:10 +0100

    Add TEMP_PATH to serve.py

--- scripts/serve.py
+++ scripts/serve.py
@@ -1,9 +1,10 @@
 from marpodb.server import app
 
 if __name__ == '__main__':
+	print app.instance_path
 	app.config['INTERPRO_PATH'] = ""
 	app.config['BLASTDB_PATH']	= ""
 	app.config['BLAST_PATH']	= ""
 	app.config["FASTA_PATH"]	= ""
-	app.config["TEMP_PATH"]	= ""
+	app.config["TEMP_PATH"]		= ""
 	app.run(debug=True,host='0.0.0.0', port = 8084)
