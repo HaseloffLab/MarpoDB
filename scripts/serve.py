e8cf637969132dabc29159638e812735a618f8c7
Mihails Delmans <md656@cam.ac.uk>
2018-08-10 15:22:47 +0100

    Clean serve.py

--- scripts/serve.py
+++ scripts/serve.py
@@ -1,10 +1,9 @@
 from marpodb.server import app
 
 if __name__ == '__main__':
-	print app.instance_path
-	app.config['INTERPRO_PATH'] = "/Users/md/MarpoDB4/data/marpodb4/marpodb4_cam_tak_2018_06_27_interpro"
-	app.config['BLASTDB_PATH']	= "/Users/md/MarpoDB4/data/marpodb4/blastdb"
-	app.config['BLAST_PATH']	= "/usr/local/ncbi/blast/bin/"
-	app.config["FASTA_PATH"]	= "/Users/md/MarpoDB4/data/marpodb4/fasta"
-	app.config["TEMP_PATH"]	= "/Users/md/MarpoDB4/data/temp"
+	app.config['INTERPRO_PATH'] = ""
+	app.config['BLASTDB_PATH']	= ""
+	app.config['BLAST_PATH']	= ""
+	app.config["FASTA_PATH"]	= ""
+	app.config["TEMP_PATH"]	= ""
 	app.run(debug=True,host='0.0.0.0', port = 8084)
